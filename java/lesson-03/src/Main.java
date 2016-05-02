2d707dce98aa7d727b4d7fb270af10f0d8c901ed
crazyacking <crazyacking@gmail.com>
2016-05-02 17:41:05 +0800

    push up

--- java/lesson-03/src/Main.java
+++ java/lesson-03/src/Main.java
@@ -3,3 +3,33 @@ public class Main {
         System.out.println("Hello World!");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
+
